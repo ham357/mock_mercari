6444526dd4fd949102c74424c38437130b097baf
ONAGAX <onagaway@gmail.com>
2019-04-13 21:06:22 +0900

    コメント消去

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,6 +1,5 @@
 $(function(){
     $(".btn-default").on("click", function(){
-        // $(this).css('background-color','gray');
         $(this).animate({backgroundColor: "gray"},300);
     });
 })
