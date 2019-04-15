8903641bf0f5d2f4fccd790cef44b88e65ac3cfa
itakurayuta1984 <48277451+itakurayuta1984@users.noreply.github.com>
2019-04-15 19:59:57 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,11 +5,6 @@ FactoryBot.define do
     email                 {"kkke@gmail.com"}
     password              {"00000000"}
     password_confirmation {"00000000"}
-    # association :user_infos, factory: :user_info
-
-  #   after(:create) do |user|
-  #     create_list(:user_info, 1, user: user)
-  #   end
   end
 
 end
