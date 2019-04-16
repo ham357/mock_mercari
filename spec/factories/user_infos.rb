a56c4e83680d5af0bdf2728230f5dc95ed0cd7f9
itakurayuta1984 <48277451+itakurayuta1984@users.noreply.github.com>
2019-04-16 20:42:49 +0900

    Update user_infos.rb

--- spec/factories/user_infos.rb
+++ spec/factories/user_infos.rb
@@ -1,22 +1,30 @@
 FactoryBot.define do
 
   factory :user_info do
-    first_name                   {"坂田"}
-    last_name                    {"稔"}
-    kana_first_name              {"サカタ"}
-    kana_last_name               {"ミノル"}
-    postal_code                  {"2850085"}
+    gimei = Gimei.name
+    gimei_kanafirstname = gimei.first.katakana
+    gimei_kanalastname = gimei.last.katakana
+    gimei_address = Gimei.address
+    gimei_state = gimei_address.prefecture.to_s
+    gimei_city = gimei_address.city.to_s
+
+
+    first_name                   { Faker::Japanese::Name.first_name }
+    last_name                    { Faker::Japanese::Name.last_name }
+    kana_first_name              { gimei_kanafirstname }
+    kana_last_name               { gimei_kanalastname }
+    postal_code                  { Faker::Address.postcode }
     state                        {"日本"}
-    city                         {"東京"}
-    adress                       {"千葉県佐倉市"}
-    tel_number                   {"09054090990"}
-    birth_year                   {"1900"}
-    birth_month                  {"12"}
-    birth_day                    {"12"}
-    profile_comment              {"こんにちは"}
-    user_id                      {"100"}
-    created_at                   {"2019-02-17 00:00:00"}
-    update_at                   {"2019-02-17 00:00:00"}
+    city                         { gimei_state}
+    adress                       { gimei_city }
+    tel_number                   { Faker::PhoneNumber.phone_number }
+    birth_year                   { Faker::Number.between(1900, 2019)}
+    birth_month                  { Faker::Number.between(1, 12) }
+    birth_day                    { Faker::Number.between(1, 31) }
+    profile_comment              { Faker::Lorem.paragraph }
+    user_id                      { Faker::Number.between(1, 30000) }
+    created_at                   { Faker::Time.between(2.days.ago, Date.today, :all) }
+    update_at                    { Faker::Time.between(2.days.ago, Date.today, :all) }
   end
 
 end
