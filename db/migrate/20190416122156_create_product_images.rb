ebeb25941ec28d46a92fa4a36d954b9dc3f064d9
ham357 <sakuraniumarete@gmail.com>
2019-04-16 22:14:13 +0900

    fix schema.rb
    マージするために一旦schema.rbをコミット

--- db/migrate/20190416122156_create_product_images.rb
+++ db/migrate/20190416122156_create_product_images.rb
@@ -2,7 +2,7 @@ class CreateProductImages < ActiveRecord::Migration[5.0]
   def change
     create_table :product_images do |t|
       t.text          :image_url,          null:false
-      t.integer       :product_id,         null:false
+      t.references     :product,           foreign_key: true
       t.timestamps
     end
   end
