767652f782346e310e75203c08c292be72b58ecd
ham357 <sakuraniumarete@gmail.com>
2019-04-17 15:21:00 +0900

    change トップページ ルーティング
    products#indexをトップページに変更しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homes#index'
+  root 'products#index'
   resources :mypages, only: :index
   resources :profiles, only: :index
   #profilesはmypagesのネスト
@@ -10,7 +10,5 @@ Rails.application.routes.draw do
   resources :card_creates, only: :index
   resources :identifacations, only: :index
   #identificationはmypagesのネスト
-
-  resources :products, only: :index
-
+  
 end
