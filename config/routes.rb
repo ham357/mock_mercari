9ee681f2d94541850220faa4c3991737ebad9d59
ONAGAX <onagaway@gmail.com>
2019-04-19 16:51:25 +0900

    コーディング完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#index'
-  resources :mypages, only: :index
-  resources :profiles, only: :index
-  #profilesはmypagesのネスト
-  resources :cards, only: :index
-  #cardsはmypagesのネスト
-  resources :card_creates, only: :index
-  resources :identifacations, only: :index
+  resources :users do
+    resources :mypages, only: :index
+    resources :profiles, only: :index
+    resources :cards, only: :index
+    resources :card_creates, only: :index
+    resources :identifacations, only: :index
+  end
   #identificationはmypagesのネスト
-
-  resources :products, only: :index
 
 end
