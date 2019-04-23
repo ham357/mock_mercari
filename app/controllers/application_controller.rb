d5ef844bc9805fb55a97cffa455764b8e9f08043
ONAGAX <onagaway@gmail.com>
2019-04-23 18:20:45 +0900

    指摘箇所を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-  root_path
+    root_path
   end
 
   private
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
--- spec/factories/user_image.rb
+++ spec/factories/user_image.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :user_image do
-    image_url             {File.open("#{Rails.root}/public/images/no_image.jpg")}
+    image_url {File.open("#{Rails.root}/public/images/no_image.jpg")}
     user
   end
 
