218b0d20dfc79c7601a78b7e29d51ce800df0306
ham357 <sakuraniumarete@gmail.com>
2019-04-25 22:56:01 +0900

    fix ルーティングの設定
    出品ページとして「/sell」のリクエストがあった際に「products#new」を呼び出すように設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
+  resources :products, only: [:index, :create, :new]
+  get 'sell', to: 'products#new'
   resources :category, only: :index
   resources :mypages, only: :index
   resources :profiles, only: :index
