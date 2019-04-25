c032a148d42e394f9687d94e7a8df2bfabb3a342
ham357 <sakuraniumarete@gmail.com>
2019-04-25 23:07:40 +0900

    add raylout sell
    application.html.hamlを呼び出さずにsell.html.hamlを呼び出すように設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,11 @@ class ProductsController < ApplicationController
         @products = Product.all
     end
 
+    def new
+        @product = Product.new
+        # @category = Category.where(["sub_category_id = ? and sub_subcategory_id = ?", "Null", "Null"])
+        @category = Category.where(sub_category_id: nil)
+        render layout: "sell"
+    end
 
 end
