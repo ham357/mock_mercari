319b412831a169f3fb8e10c3144bc7bef0f791fb
ONAGAX <onagaway@gmail.com>
2019-04-30 20:57:01 +0900

    商品状態実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,4 +17,5 @@ class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
+  belongs_to_active_hash :status
 end
