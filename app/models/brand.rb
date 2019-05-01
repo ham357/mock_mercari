08dbb3b8410ba620c8bba38338347f0d8e2d362b
ham357 <sakuraniumarete@gmail.com>
2019-05-01 21:32:18 +0900

    del リレーションとバリデーション
    不要な記載を削除いたしました。

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,3 @@
 class Brand < ApplicationRecord
   validates :name, presence: true
-  validates :name, uniqueness: true
-  belongs_to :product
 end
--- app/models/product_size.rb
+++ app/models/product_size.rb
@@ -1,6 +1,3 @@
 class ProductSize < ApplicationRecord
   validates :name, presence: true
-
-  belongs_to :product
-  belongs_to :category
 end
