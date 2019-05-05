b5b04b8463e9335c381ac2f9b2c1929496196e0f
ham357 <sakuraniumarete@gmail.com>
2019-05-05 19:22:29 +0900

    add 画像ドロップアンドドロップの実装
    画像ドロップアンドドロップの実装を行いました。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,31 +7,37 @@ class ProductsController < ApplicationController
     def new
         @product = Product.new
         @product.product_images.build
-        # @categories = Category.where(sub_category_id: nil)
         render layout: "sell"
     end
 
     def create
         @product = Product.new(product_params)
-        # @product.save
-        respond_to do |format|
-            if @product.save
-                binding.pry
-                params[:product_images][:image_url].each do |image|
-                    @product.product_images.create(image_url: image, product_id: @product.id)
-                end
-                format.html{redirect_to root_path}
-            else
-                binding.pry
-                @product.product_images.build
-                # @categories = Category.where(sub_category_id: nil)
+        if @product.save
+            create_itemimage(@product.id)
+            respond_to do |format|
+                format.html { redirect_to root_path }
+                format.json
+            end
+        else
+            @product.product_images.build
+            respond_to do |format|
                 format.html{render action: 'new'}
+                format.json
             end
         end
     end
 
     def product_params
         params.require(:product).permit(:name,:status,:shipping_fee,:state,:shipping_method,:shipping_day,:price,:size,:description,:category_id,:brand_id, product_images_attributes: [:image_url]).merge(user_id:1)
+    end
+
+    def create_itemimage(product_id)
+        i = 0
+        while params[:product][:product_images_attributes][:image_url][i.to_s].present? do
+            image = params[:product][:product_images_attributes][:image_url][i.to_s]
+            ProductImage.create(product_id: product_id, image_url: image)
+            i += 1
+        end
     end
 
 end
