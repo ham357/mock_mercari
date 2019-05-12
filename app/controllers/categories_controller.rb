6fd85589c2ed47105f2543862556f689d2d9c083
ONAGAX <onagaway@gmail.com>
2019-05-13 02:23:42 +0900

    大まかなカテゴリー検索完了

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,10 @@
 class CategoriesController < ApplicationController
-  class CategoriesController < ApplicationController
-    def index
-      # カテゴリー一覧ページ
-      @categories = Category.all
-      @main_categories = Category.where(sub_category_id: nil, sub_subcategory_id: nil)
-      @sub_categories = Category.where(sub_category_id: presence, sub_subcategory_id: nil)
-      @sub_subcategories = Category.where(sub_category_id: presence, sub_subcategory_id: presence)
-    end
-  
-    def show
-      # カテゴリー詳細ページ
-    end
+  def index
+    @main_categories = Category.eager_load(children: :children).where(sub_category_id: nil)
+    @sub_categories = Category.eager_load(children: {children: :children}).where(sub_category_id: nil)
+  end
+
+  def show
+    # カテゴリー詳細ページ
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,11 @@
 class Category < ApplicationRecord
   validates :name, presence: true
   has_one :product_size
+
+  belongs_to :parent, class_name: :Category
+  has_many :children,class_name: :Category, foreign_key: :main_category_id
+
+  def self.get_array_subsub(main,sub)
+    array = Category.where(main_category_id: main).where(sub_category_id: sub)
+  end
 end
