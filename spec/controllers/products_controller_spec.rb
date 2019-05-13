e8457c1c96d6a7754e514e3564bfec9883c6ca2e
ham357 <sakuraniumarete@gmail.com>
2019-05-13 23:04:24 +0900

    fix 共通部分のまとめ
    繰り返し記載されている共通部分をまとめました。

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe ProductsController, type: :controller do
   let(:user) { create(:user)}
+  let(:product) { create(:product,user_id:user.id)}
+  let(:product_image) { create_list(:product_image, 4, product_id: product.id)}
   let(:update_attributes) do
     {
         price: 99999
@@ -13,7 +15,7 @@ describe ProductsController, type: :controller do
   
   describe 'GET #index' do
     it "インスタンス変数の値が正常" do
-      products = create_lis(:product, 3,user_id: user.id)
+      products = create_list(:product, 3,user_id: user.id)
       get :index
       expect(assigns(:products)).to match(products)
     end
@@ -46,33 +48,25 @@ describe ProductsController, type: :controller do
 
   describe '#destroy' do
     it "商品が削除されているか" do
-      product = create(:product,user_id:user.id)
-      4.times{ create(:product_image, product_id: product.id) }
       expect{
-        delete :destroy, id: product
+        delete :destroy, params: { id: product.id, product: attributes_for(:product) }
       }.to change(Product,:count).by(-1)
     end
   end
 
   describe '#update' do
     it "正常に変数にテストデータが入っているか" do
-      product = create(:product,user_id:user.id)
-      4.times{ create(:product_image, product_id: product.id) }
       patch :update, params: { id: product.id, product: attributes_for(:product) }
       expect(assigns(:product)).to eq product
     end
 
     it "価格が更新できているか" do
-      product = create(:product,user_id:user.id)
-      4.times{ create(:product_image, product_id: product.id) }
       patch :update, params: { id: product.id, product: update_attributes }
       product.reload
       expect(product.price).to eq(99999)
     end
 
     it "正常にトップページへリダイレクトされているか" do
-      product = create(:product,user_id:user.id)
-      4.times{ create(:product_image, product_id: product.id) }
       patch :update, params: { id: product.id, product: update_attributes }
       expect(response).to redirect_to root_path
     end
