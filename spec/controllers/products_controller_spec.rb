2def545e115cc2a0709151058130f492a3a0de81
ham357 <sakuraniumarete@gmail.com>
2019-05-13 21:43:42 +0900

    fix コメントアウトの削除
    一旦コメントアウトしていた記載を解除しました。

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -11,48 +11,48 @@ describe ProductsController, type: :controller do
     sign_in user
   end
   
-  # describe 'GET #index' do
-  #   it "インスタンス変数の値が正常" do
-  #     products = create_list(:product, 3,user_id: user.id)
-  #     get :index
-  #     expect(assigns(:products)).to match(products)
-  #   end
+  describe 'GET #index' do
+    it "インスタンス変数の値が正常" do
+      products = create_lis(:product, 3,user_id: user.id)
+      get :index
+      expect(assigns(:products)).to match(products)
+    end
 
-  #   it "ビューに正しく遷移できる" do
-  #     get :index
-  #     expect(response).to render_template :index
-  #   end
-  # end
-  # describe '#show' do
-  #   context 'viewが見えてるか' do
-  #     it 'showのテンプレートが表示されてるか' do
-  #       product = create(:product, user_id: user.id, name: 'あああ')
-  #       product_image = create(:product_image, product_id: product.id)
-  #       category = create(:category)
-  #       brand = create(:brand)
-  #       get :show, params: {id: product.id}
-  #       expect(response).to render_template :show
-  #     end
-  #     it 'タイトルが出てるか' do
-  #       product = create(:product, user_id: user.id, name: 'あああ')
-  #       product_image = create(:product_image, product_id: product.id)
-  #       category = create(:category)
-  #       brand = create(:brand)
-  #       get :show, params: {id: product.id}
-  #       expect(assigns(:product).name).to eq "あああ"
-  #     end
-  #   end
-  # end
+    it "ビューに正しく遷移できる" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+  describe '#show' do
+    context 'viewが見えてるか' do
+      it 'showのテンプレートが表示されてるか' do
+        product = create(:product, user_id: user.id, name: 'あああ')
+        product_image = create(:product_image, product_id: product.id)
+        category = create(:category)
+        brand = create(:brand)
+        get :show, params: {id: product.id}
+        expect(response).to render_template :show
+      end
+      it 'タイトルが出てるか' do
+        product = create(:product, user_id: user.id, name: 'あああ')
+        product_image = create(:product_image, product_id: product.id)
+        category = create(:category)
+        brand = create(:brand)
+        get :show, params: {id: product.id}
+        expect(assigns(:product).name).to eq "あああ"
+      end
+    end
+  end
 
-  # describe '#destroy' do
-  #   it "商品が削除されているか" do
-  #     product = create(:product,user_id:user.id)
-  #     4.times{ create(:product_image, product_id: product.id) }
-  #     expect{
-  #       delete :destroy, id: product
-  #     }.to change(Product,:count).by(-1)
-  #   end
-  # end
+  describe '#destroy' do
+    it "商品が削除されているか" do
+      product = create(:product,user_id:user.id)
+      4.times{ create(:product_image, product_id: product.id) }
+      expect{
+        delete :destroy, id: product
+      }.to change(Product,:count).by(-1)
+    end
+  end
 
   describe '#update' do
     it "正常に変数にテストデータが入っているか" do
