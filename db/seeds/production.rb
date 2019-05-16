ad5bcdba34feafa7d89ea8c2a944ab6b3d6f2786
ONAGAX <onagaway@gmail.com>
2019-05-16 10:42:26 +0900

    seed用

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,5 +1,18 @@
 require "csv"
 
+CSV.foreach('db/csv/product_shipping_fee_data.csv', headers: true) do |row|
+  ProductShippingFee.create(
+      name: row["name"]
+  )
+end
+
+CSV.foreach('db/csv/product_shipping_method_data.csv', headers: true) do |row|
+  ProductShippingMethod.create(
+      name: row["name"],
+      shipping_fee_category: row["shipping_fee_category"]
+  )
+end
+
 CSV.foreach('db/csv/brand_data.csv', headers: true) do |row|
   Brand.create(name: row['name']
                   )
@@ -20,18 +33,5 @@ CSV.foreach('db/csv/product_size_data.csv', headers: true) do |row|
   ProductSize.create(
       name: row["name"],
       product_size_id: row["product_size_id"]
-  )
-end
-
-CSV.foreach('db/csv/product_shipping_fee_data.csv', headers: true) do |row|
-  ProductShippingFee.create(
-      name: row["name"]
-  )
-end
-
-CSV.foreach('db/csv/product_shipping_method_data.csv', headers: true) do |row|
-  ProductShippingMethod.create(
-      name: row["name"],
-      shipping_fee_category: row["shipping_fee_category"]
   )
 end
