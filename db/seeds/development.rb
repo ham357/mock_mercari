afd348db1f041c889952ab5e7c2f4b77e141a393
ONAGAX <onagaway@gmail.com>
2019-05-16 09:59:10 +0900

    SEED用

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -162,3 +162,16 @@ CSV.foreach('db/csv/product_size_data.csv', headers: true) do |row|
       product_size_id: row["product_size_id"]
   )
 end
+
+CSV.foreach('db/csv/product_shipping_fee_data.csv', headers: true) do |row|
+  ProductShippingFee.create(
+      name: row["name"]
+  )
+end
+
+CSV.foreach('db/csv/product_shipping_method_data.csv', headers: true) do |row|
+  ProductShippingMethod.create(
+      name: row["name"],
+      shipping_fee_category: row["shipping_fee_category"]
+  )
+end
