cee22267d904cb10ea95543c3b2db231feb75400
ham357 <sakuraniumarete@gmail.com>
2019-05-21 19:51:54 +0900

    add 作成中のファイルを一旦コミット
    マージするため一旦コミットします。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190516124359) do
+ActiveRecord::Schema.define(version: 20190514112049) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "prefecture_id"
@@ -34,19 +34,15 @@ ActiveRecord::Schema.define(version: 20190516124359) do
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                               null: false
-    t.integer  "main_category_id",                   null: false
+    t.string   "name",               null: false
+    t.integer  "main_category_id",   null: false
     t.integer  "sub_category_id"
     t.integer  "sub_subcategory_id"
     t.integer  "product_size_id"
-    t.boolean  "size_flag",          default: false, null: false
-    t.boolean  "brand_flag",         default: false, null: false
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "pre_category_id"
     t.integer  "pre_precategory_id"
-    t.boolean  "size_flag"
-    t.boolean  "brand_flag"
     t.index ["name"], name: "index_categories_on_name", using: :btree
   end
 
@@ -121,7 +117,10 @@ ActiveRecord::Schema.define(version: 20190516124359) do
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                             null: false
     t.integer  "status_id",                                        null: false
+    t.integer  "shipping_fee_id",                                  null: false
+    t.integer  "state_id",                                         null: false
     t.integer  "shipping_method_id",                               null: false
+    t.integer  "shipping_day_id",                                  null: false
     t.integer  "price",                                            null: false
     t.text     "description",        limit: 65535,                 null: false
     t.integer  "category_id",                                      null: false
@@ -131,9 +130,6 @@ ActiveRecord::Schema.define(version: 20190516124359) do
     t.datetime "created_at",                                       null: false
     t.datetime "updated_at",                                       null: false
     t.integer  "product_size_id"
-    t.integer  "state_id"
-    t.integer  "shipping_day_id"
-    t.integer  "shipping_fee_id"
     t.index ["name"], name: "index_products_on_name", using: :btree
     t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
