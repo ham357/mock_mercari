c3dd2d308ffdb3f0a006f6fe38b90a15fe338086
ham357 <sakuraniumarete@gmail.com>
2019-06-29 00:55:58 +0900

    fix AWS S3バケット名
    AWS S3バケット名の修正を行いました。

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ham-ipranges'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bucket'
+  config.fog_directory  = 'upload-inoue-test'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-inoue-test
 end
